d59fddbb416ff613af63d540d3f1076de0c79bc0
Michael Russo <mjrusso@gmail.com>
2012-05-05 23:51:38 -0400

    more progress on chapter 11

--- src/joc/ch11.clj
+++ src/joc/ch11.clj
@@ -7,6 +7,8 @@
 ;; - Vars:   thread-local storage
 ;; All but Vars are considered shared references (and allow for
 ;; changes to be seen across threads of execution).
+;; Values can be accessed via the `@` reader macro, or the `deref`
+;; function (regardless of the reference type).
 
 (doc io!)
 
@@ -21,7 +23,6 @@
 ;; (dosync (io! (.println System/out "Haikeeba!")))
 
 ;; Used to help illustrate some examples.
-
 (import '(java.util.concurrent Executors))
 (def *pool* (Executors/newFixedThreadPool
             (+ 2 (.availableProcessors (Runtime/getRuntime)))))
@@ -31,7 +32,169 @@
   (dotimes [t thread-count]
     (.submit *pool* #(dotimes [_ exec-count] (f)))))
 
-;; TODO come back to 11.2 Refs
+;; Refs
+;; Refs are coordinated (reads and writes to mulitple refs can be made
+;; in a way that guarantees no race conditions).
+;; Refs are retriable (the work done to update a ref's value is
+;; speculative and may have to be repeated).
+
+;; Clojure enforces that any change to a Ref's value occurs in a
+;; transaction, maintaining a consistent view of the referenced value
+;; in all threads.
+
+;; A 3x3 chess board. The act of moving a piece requires a
+;; *coordinated* change in two reference squares.
+(def initial-board
+  [[:- :k :-]
+   [:- :- :-]
+   [:- :K :-]])
+
+(defn board-map [f bd]
+  (vec (map #(vec (for [s %] (f s))) bd)))
+
+(defn reset!
+  "Resets the board state.  Generally these types of functions are a
+   bad idea, but matters of page count force our hand."
+  []
+  (def board (board-map ref initial-board))
+  (def to-move (ref [[:K [2 1]] [:k [0 1]]]))
+  (def num-moves (ref 0)))
+
+(defn neighbors
+  ([size yx] (neighbors [[-1 0] [1 0] [0 -1] [0 1]] size yx))
+  ([deltas size yx]
+     (filter (fn [new-yx]
+               (every? #(< -1 % size) new-yx))
+     (map #(map + yx %) deltas))))
+
+(def king-moves (partial neighbors
+                         [[-1 -1] [-1 0] [-1 1] [0 -1] [0 1] [1 -1] [1 0] [1 1]] 3))
+
+(defn good-move? [to enemy-sq]
+  (when (not= to enemy-sq) to))
+
+(defn choose-move [[[mover mpos][_ enemy-pos]]]
+  [mover (some #(good-move? % enemy-pos)
+               (shuffle (king-moves mpos)))])
+
+(reset!)
+(take 5 (repeatedly #(choose-move @to-move)))
+@to-move
+
+(defn place [from to] to)
+
+(defn move-piece [[piece dest] [[_ src] _]]
+  (alter (get-in board dest) place piece)
+  (alter (get-in board src ) place :-)
+  (alter num-moves inc))
+
+(defn update-to-move [move]
+  (alter to-move #(vector (second %) move)))
+
+(defn make-move []
+  (dosync
+    (let [move (choose-move @to-move)]
+      (move-piece move @to-move)
+      (update-to-move move))))
+
+(make-move)
+(board-map deref board)
+(deref num-moves)
+@num-moves
+
+(defn go [move-fn threads times]
+  (dothreads! move-fn :threads threads :times times))
+
+(go make-move 100 100)
+(board-map #(dosync (deref %)) board)
+@to-move
+@num-moves
+
+;; Bad: we shouldn't allow the Ref updates to happen in separate
+;; transactions
+(defn bad-make-move []
+  (let [move (choose-move @to-move)]
+    (dosync (move-piece move @to-move))
+    (dosync (update-to-move move))))
+
+(go bad-make-move 100 100)
+(board-map #(dosync (deref %)) board)
+
+;; Using `alter` can cause a transation to retry if a Ref it depends
+;; on is modified and committed while it is running. `commute` can be
+;; used in cases where the value of a Ref within the transaction is
+;; not important to its completion semantics (e.g. `num-moves`, which
+;; is a simple counter: its value at any given time is not important,
+;; as long as it is incremented properly at the end of the transaction).
+(doc commute)
+
+(defn move-piece [[piece dest] [[_ src] _]]
+  (commute (get-in board dest) place piece)
+  (commute (get-in board src ) place :-)
+  (commute num-moves inc))
+
+(reset!)
+(go make-move 100 100)
+(board-map deref board)
+@to-move
+
+;; Use `commute` only if the following is acceptable:
+;; - the value seen in-transaction may not be the value committed at
+;;   commit time
+;; - the function given to commute will be run at least twice (first,
+;;   to compute the in-transaction value, and again to commute the
+;;   commit value)
+
+;; `ref-set` changes a value of a Ref to the raw value provided
+;; (differs from `alter` and `commute`, which change a Ref based on a
+;; function of its value). Not recommended.
+
+(dosync (ref-set to-move '[[:K [2 1]] [:k [0 1]]]))
+@to-move
+
+;; Snapshot isolation: within a transaction, all enclosed Ref states
+;; represent the same moment in time. Any Ref value inside a
+;; transaction will never change, unless you explicitly change it
+;; within that transaction. (If the values have changed, then the
+;; transaction retries.)
+;; Write skew: occurs when a decision is made inside of a transaction,
+;; based on the in-transaction value of a Ref that is never written
+;; to, is changed at the same time. The `ensure` function can be used
+;; to avoid write skew.
+;; `ensure` guarantees that a read-only Ref is not modified in another
+;; thread.
+(doc ensure)
+
+;; As a rule of thumb, avoid having both short- and long-running
+;; transactions interacting with the same Ref.
+
+;; Stress a Ref -- mixes long-running (slow) transactions and quick
+;; transactions.
+(defn stress-ref [r]
+  (let [slow-tries (atom 0)]
+    (future
+      (dosync
+       (swap! slow-tries inc)
+       (Thread/sleep 200)
+       @r)
+      (println (format "r is: %s, history: %d, after: %d tries"
+                       @r (ref-history-count r) @slow-tries)))
+    (dotimes [i 500]
+      (Thread/sleep 10)
+      (dosync (alter r inc)))
+    :done))
+
+(stress-ref (ref 0))
+(stress-ref (ref 0 :max-history 30))
+(stress-ref (ref 0 :min-history 15 :max-history 30))
+
+(def my-ref-1 (ref 9))
+(def my-ref-2 (ref 2)))))))
+(dosync
+ (alter my-ref-1 inc)
+ (alter my-ref-2 - 3 5)
+ (alter my-ref-2 inc))
+(map deref [my-ref-1 my-ref-2])
 
 ;; Agents
 
